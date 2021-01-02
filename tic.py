d3ee3c8703425093a74dddd239bc09d0957527a8
VijayKrishna R <73846668+ViKi-R@users.noreply.github.com>
2021-01-03 01:26:29 +0530

    Updated

--- tic.py
+++ tic.py
@@ -1,39 +1,30 @@
-'''
-████████ ██  ██████     ████████  █████   ██████     ████████  ██████  ███████
-   ██    ██ ██             ██    ██   ██ ██             ██    ██    ██ ██
-   ██    ██ ██             ██    ███████ ██             ██    ██    ██ █████
-   ██    ██ ██             ██    ██   ██ ██             ██    ██    ██ ██
-   ██    ██  ██████        ██    ██   ██  ██████        ██     ██████  ███████
-'''
 import os
 
 s = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 k = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 count = 0
-player = 'ESC'
+player = null
 
 
 def draw():
     print("\n")
     print('''
-        ######## ##  ######     ########  #####   ######     ########  ######  #######
-           ##    ## ##             ##    ##   ## ##             ##    ##    ## ##
-           ##    ## ##             ##    ####### ##             ##    ##    ## #######
-           ##    ## ##             ##    ##   ## ##             ##    ##    ## ##
-           ##    ##  ######        ##    ##   ##  ######        ##     ######  #######
+        ▀▀█▀▀ ▀█▀ ▒█▀▀█   ▀▀█▀▀ ░█▀▀█ ▒█▀▀█   ▀▀█▀▀ ▒█▀▀▀█ ▒█▀▀▀ 
+         ▒█░░ ▒█░ ▒█░░░   ░▒█░░ ▒█▄▄█ ▒█░░░   ░▒█░░ ▒█░░▒█ ▒█▀▀▀ 
+         ▒█░░ ▄█▄ ▒█▄▄█   ░▒█░░ ▒█░▒█ ▒█▄▄█   ░▒█░░ ▒█▄▄▄█ ▒█▄▄▄
         ''')
-    print("\033[10;36HCreator: D4RK C0D3R \n")
+    print("\033[8;9H | Creator: ViKi-R | Website:https://viki-r.github.io/ | \n")
     print("\033[12;40H" + str(s[0]) + " | " + str(s[1]) + " | " + str(s[2]))
     print("\033[13;39H" + "---+---+---")
-    print("\033[16;40H" + str(s[3]) + " | " + str(s[4]) + " | " + str(s[5]))
-    print("\033[18;39H" + "---+---+---")
-    print("\033[20;40H" + str(s[6]) + " | " + str(s[7]) + " | " + str(s[8]))
-    print('\n')
+    print("\033[14;40H" + str(s[3]) + " | " + str(s[4]) + " | " + str(s[5]))
+    print("\033[15;39H" + "---+---+---")
+    print("\033[16;40H" + str(s[6]) + " | " + str(s[7]) + " | " + str(s[8]))
+    print("\033[18;40H")
 
 
 def TogglePlayer(p1, p2):
     global player
-    if(player == p1):
+    if player == p1:
         player = p2
     else:
         player = p1
@@ -43,58 +34,58 @@ def field():
     a = int(input("Choose the Number " + player + ": "))
     global count
     count += 1
-    if(a == 1):
+    if a == 1:
         s[0] = player
-    elif(a == 2):
+    elif a == 2:
         s[1] = player
-    elif(a == 3):
+    elif a == 3:
         s[2] = player
-    elif(a == 4):
+    elif a == 4:
         s[3] = player
-    elif(a == 5):
+    elif a == 5:
         s[4] = player
-    elif(a == 6):
+    elif a == 6:
         s[5] = player
-    elif(a == 7):
+    elif a == 7:
         s[6] = player
-    elif(a == 8):
+    elif a == 8:
         s[7] = player
-    elif(a == 9):
+    elif a == 9:
         s[8] = player
 
 
 def check():
     global player
-    if (s[0] == s[1] and s[1] == s[2]):
+    if s[0] == s[1] and s[1] == s[2]:
         return player
-    if (s[3] == s[4] and s[4] == s[5]):
+    if s[3] == s[4] and s[4] == s[5]:
         return player
-    if (s[6] == s[7] and s[7] == s[8]):
+    if s[6] == s[7] and s[7] == s[8]:
         return player
-    if (s[0] == s[3] and s[3] == s[6]):
+    if s[0] == s[3] and s[3] == s[6]:
         return player
-    if (s[1] == s[4] and s[4] == s[7]):
+    if s[1] == s[4] and s[4] == s[7]:
         return player
-    if (s[2] == s[5] and s[5] == s[8]):
+    if s[2] == s[5] and s[5] == s[8]:
         return player
-    if (s[0] == s[4] and s[4] == s[8]):
+    if s[0] == s[4] and s[4] == s[8]:
         return player
-    if (s[2] == s[4] and s[4] == s[6]):
+    if s[2] == s[4] and s[4] == s[6]:
         return player
-    if(count == 9):
+    if count == 9:
         return 'd'
     return '/'
 
 
 def winner(d):
     global a, b
-    if(d == a):
+    if d == a:
         print(a + " Wins!! ()==|:::::::::::> Game Over\n")
         return 1
-    if(d == b):
+    if d == b:
         print(b + " Wins!! ()==|:::::::::::> Game Over\n")
         return 1
-    if(d == 'd'):
+    if d == 'd':
         print("Draw!! ()==|:::::::::::> Game Over\n")
         return 1
 
@@ -107,11 +98,11 @@ def playagain():
     print("Do you want to play again:")
     print("Y) Yes\nN) No")
     choice = input("Choose: ")
-    if(choice == 'Y' or choice == 'y' or choice == '1'):
+    if choice == 'Y' or choice == 'y' or choice == '1':
         for i in s:
             s[i] = k[i]
         main()
-    if(choice == 'N' or choice == 'n' or choice == '2'):
+    if choice == 'N' or choice == 'n' or choice == '2':
         clear()
         os.system("exit")
 
@@ -123,7 +114,7 @@ def main():
     draw()
     a = input("Enter Player One's character: ")
     b = input("Enter Player Two's character: ")
-    while(x != 1):
+    while x != 1:
         clear()
         draw()
         TogglePlayer(a, b)
@@ -133,7 +124,7 @@ def main():
         draw()
         x = winner(c)
 
-    if(x == 1):
+    if x == 1:
         playagain()
 
 
